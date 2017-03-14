0de929255697a32fee54ed52d02ac3fff4b1aa43
IOS Dev <ios.dev.21@outlook.com>
2017-03-14 18:33:27 -0400

    Firebase database
    add the grocery-item to database

---
+++ Grocery/GroceryListTableViewController.swift
@@ -0,0 +1,155 @@
+//
+//  GroceryListTableViewController.swift
+//  Grocery
+//
+//  Created by Admin on 3/14/17.
+//  Copyright © 2017 harry. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class GroceryListTableViewController: UITableViewController {
+    
+    
+    let ref = FIRDatabase.database().reference(withPath: "grocery-items")
+    
+    // MARK: Constants1
+    let listToUsers = "ListToUsers"
+    
+    // MARK: Properties
+    var items: [GroceryItem] = []
+    var user: User!
+    var userCountBarButtonItem: UIBarButtonItem!
+
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        tableView.allowsMultipleSelectionDuringEditing = false
+        
+        userCountBarButtonItem = UIBarButtonItem(title: "1",
+                                                 style: .plain,
+                                                 target: self,
+                                                 action: #selector(userCountButtonDidTouch))
+        userCountBarButtonItem.tintColor = UIColor.white
+        navigationItem.leftBarButtonItem = userCountBarButtonItem
+        
+        user = User(uid: "FakeId", email: "hungry@person.food")
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: UITableView Delegate methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+        let groceryItem = items[indexPath.row]
+        
+        cell.textLabel?.text = groceryItem.name
+        cell.detailTextLabel?.text = groceryItem.addedByUser
+        
+        toggleCellCheckbox(cell, isCompleted: groceryItem.completed)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            items.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        var groceryItem = items[indexPath.row]
+        let toggledCompletion = !groceryItem.completed
+        
+        toggleCellCheckbox(cell, isCompleted: toggledCompletion)
+        groceryItem.completed = toggledCompletion
+        tableView.reloadData()
+    }
+    
+    func toggleCellCheckbox(_ cell: UITableViewCell, isCompleted: Bool) {
+        if !isCompleted {
+            cell.accessoryType = .none
+            cell.textLabel?.textColor = UIColor.black
+            cell.detailTextLabel?.textColor = UIColor.black
+        } else {
+            cell.accessoryType = .checkmark
+            cell.textLabel?.textColor = UIColor.gray
+            cell.detailTextLabel?.textColor = UIColor.gray
+        }
+    }
+
+    
+    // MARK: Add Item
+    
+    @IBAction func addButtonDidTouch(_ sender: AnyObject) {
+        let alert = UIAlertController(title: "Grocery Item",
+                                      message: "Add an Item",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default) { _ in
+                                        // 1
+                                        guard let textField = alert.textFields?.first,
+                                            let text = textField.text else { return }
+                                        
+                                        // 2
+                                        let groceryItem = GroceryItem(name: text,
+                                                                      addedByUser: self.user.email,
+                                                                      completed: false)
+                                        // 3
+                                        let groceryItemRef = self.ref.child(text.lowercased())
+                                        
+                                        // 4
+                                        groceryItemRef.setValue(groceryItem.toAnyObject())
+                                        
+                                        self.items.append(groceryItem)
+                                        self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .default)
+        
+        alert.addTextField()
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func userCountButtonDidTouch() {
+        performSegue(withIdentifier: listToUsers, sender: nil)
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Grocery/LoginViewController.swift
@@ -0,0 +1,78 @@
+//
+//  LoginViewController.swift
+//  Grocery
+//
+//  Created by Admin on 3/14/17.
+//  Copyright © 2017 harry. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+ 
+    // MARK: Constants
+    let loginToList = "LoginToList"
+    
+    // MARK: Outlets
+    @IBOutlet weak var textFieldLoginEmail: UITextField!
+    @IBOutlet weak var textFieldLoginPassword: UITextField!
+    
+    // MARK: Actions
+    @IBAction func loginDidTouch(_ sender: AnyObject) {
+        performSegue(withIdentifier: loginToList, sender: nil)
+    }
+    
+    @IBAction func signUpDidTouch(_ sender: AnyObject) {
+        let alert = UIAlertController(title: "Register",
+                                      message: "Register",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default) { action in
+                                        
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .default)
+        
+        alert.addTextField { textEmail in
+            textEmail.placeholder = "Enter your email"
+        }
+        
+        alert.addTextField { textPassword in
+            textPassword.isSecureTextEntry = true
+            textPassword.placeholder = "Enter your password"
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
